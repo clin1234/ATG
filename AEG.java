742f7dab843c488134284d277b0006ef9e3c0224
Charlie Lin <clin@rollins.edu>
2021-11-05 15:05:13 -0400

    Wrap all statements within a try-resource block.
    This squashes a spurious error message when getting input for answers:
    ```
    Exception in thread "main" java.util.NoSuchElementException: No line found
            at java.base/java.util.Scanner.nextLine(Scanner.java:1656)
            at Exam.takeExam(Exam.java:138)
            at AEG.main(AEG.java:19)
    ```

--- AEG.java
+++ AEG.java
@@ -5,21 +5,14 @@ public class AEG {
 
 	public static void main(String[] args) {
 		System.out.print("Enter your first and last name: ");
-		/*
-		String name;
 		try (Scanner s = new Scanner(System.in)) {
-			//while (s.hasNextLine())
-				name = s.nextLine();
+			String name = s.nextLine();
+			Exam e = new Exam(name, LocalDate.now().toString());
+
+			e.takeExam();
+			e.gradeExam();
+			e.displayResult();
+
 		}
-		*/
-		// XXX: hardcoding a name prevents an exception
-		// thrown by s.nextLine
-		Exam e = new Exam("LOL", LocalDate.now().toString());
-
-		e.takeExam();
-		e.gradeExam();
-		e.displayResult();
-
 	}
-
 }
