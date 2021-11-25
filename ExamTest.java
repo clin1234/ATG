a32f1b12e468f9b8fd93cec4ab36c7ca3ae4a47e
Matheus Westphalen <matheuskaram@hotmail.com>
2021-11-25 11:13:37 -0500

    Add a few test cases to Exam

--- ExamTest.java
+++ ExamTest.java
@@ -6,62 +6,123 @@ class ExamTest {
 
 	@Test
 	void testExam() {
-		fail("Not yet implemented");
+		// Call to the method being tested
+		Exam e = new Exam("Jack Black", "2021-04-28");
+		// Assertion
+		assertTrue(e instanceof Exam);
 	}
 
 	@Test
 	void testGetMAX_SCORE() {
-		fail("Not yet implemented");
+		// Setting up for testing
+		Exam e = new Exam("Erin Mack", "2021-11-09");
+		final int expected = 25;
+		// Call to the method being tested
+		final int actual = e.getMAX_SCORE();
+		// Assertion
+		assertEquals(expected, actual);
 	}
 
 	@Test
 	void testGetQUESTION_WEIGHT() {
-		fail("Not yet implemented");
+		// Setting up for testing
+		Exam e = new Exam("Archie Hen", "2021-08-12");
+		final int expected = 1;
+		// Call to the method being tested
+		final int actual = e.getQUESTION_WEIGHT();
+		// Assertion
+		assertEquals(expected, actual);
 	}
 
 	@Test
 	void testTakeExam() {
 		fail("Not yet implemented");
+		// Setting up for testing
+		
+		// Call to the method being tested
+
+		// Assertion
 	}
 
 	@Test
 	void testIsValidInputStringMultipleChoice() {
 		fail("Not yet implemented");
+		// Setting up for testing
+		
+		// Call to the method being tested
+
+		// Assertion
 	}
 
 	@Test
 	void testIsValidInputStringTrueOrFalse() {
 		fail("Not yet implemented");
+		// Setting up for testing
+		
+		// Call to the method being tested
+
+		// Assertion
 	}
 
 	@Test
 	void testGradeExam() {
 		fail("Not yet implemented");
+		// Setting up for testing
+		
+		// Call to the method being tested
+
+		// Assertion
 	}
 
 	@Test
 	void testDisplayResult() {
 		fail("Not yet implemented");
+		// Setting up for testing
+		
+		// Call to the method being tested
+
+		// Assertion
 	}
 
 	@Test
 	void testWriteOut() {
 		fail("Not yet implemented");
+		// Setting up for testing
+		
+		// Call to the method being tested
+
+		// Assertion
 	}
 
 	@Test
 	void testPrintExamResult() {
 		fail("Not yet implemented");
+		// Setting up for testing
+		
+		// Call to the method being tested
+
+		// Assertion
+
 	}
 
 	@Test
 	void testGetUserName() {
-		fail("Not yet implemented");
+		// Setting up for testing
+		Exam e = new Exam("Charlie Lin", "2021-09-20");
+		// Call to the method being tested
+		String actual = e.testGetUserName();
+		// Assertion
+		assertEquals("Charlie Lin", actual);
 	}
 
 	@Test
 	void testGetTestDate() {
-		fail("Not yet implemented");
+		// Setting up for testing
+		Exam e = new Exam("Matt Westphalen", "2021-11-17");
+		// Call to the method being tested
+		String actual = e.testGetTestDate();
+		// Assertion
+		assertEquals("2021-11-17", actual);
 	}
 
 }
